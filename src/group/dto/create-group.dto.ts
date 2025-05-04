1e5be5a9b83d4a9689820016d79b5f3e88c10da4
Ygor Paz <pazygor080@gmail.com>
2025-05-04 01:59:27 -0300

    Criando crud básico para grupos

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,1 +1,10 @@
-export class CreateGroupDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+export class CreateGroupDto {
+    @IsNotEmpty()
+    @IsString()
+    nome: string;
+
+    @IsOptional()
+    @IsString()
+    descricao?: string;
+}
--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 
 @Injectable()
 export class GroupService {
-  create(createGroupDto: CreateGroupDto) {
-    return 'This action adds a new group';
+  constructor(private prisma: PrismaService) { }
+
+  create(createGrupoDto: CreateGroupDto) {
+    return this.prisma.grupo.create({
+      data: {
+        nome: createGrupoDto.nome,
+        descricao: createGrupoDto.descricao,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all group`;
+    return this.prisma.grupo.findMany({
+      include: {
+        usuarios: true,
+        alertas: true,
+      },
+    });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} group`;
+    return this.prisma.grupo.findUnique({
+      where: { id },
+      include: {
+        usuarios: true,
+        alertas: true,
+      },
+    });
   }
 
-  update(id: number, updateGroupDto: UpdateGroupDto) {
-    return `This action updates a #${id} group`;
+  update(id: number, updateGrupoDto: UpdateGroupDto) {
+    return this.prisma.grupo.update({
+      where: { id },
+      data: updateGrupoDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} group`;
+    return this.prisma.grupo.delete({
+      where: { id },
+    });
   }
 }
